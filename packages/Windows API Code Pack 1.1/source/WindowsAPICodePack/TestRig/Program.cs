df143a156927253137ff2a545e54065d886991f3
protechdm <protechdm@yahoo.com>
2014-10-06 16:08:09 +0100

    PUSH 6

---
+++ packages/Windows API Code Pack 1.1/source/WindowsAPICodePack/TestRig/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.WindowsAPICodePack.Shell;
+
+namespace TestRig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "J:\\CloudCompare\\CloudCompare.Web\\Documents\\WhitePapers\\words.pdf";
+
+            var document = ShellObject.FromParsingName(fileName);
+
+        }
+    }
+}
